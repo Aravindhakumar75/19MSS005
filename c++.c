e06d606a2abfe0a2c7584d97b28d5bd1f7fc478c
Aravindhakumar75 <111726046+Aravindhakumar75@users.noreply.github.com>
2023-06-09 19:53:58 +0530

    Add files

---
+++ c++.c
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<json>
+
+using namespace std;
+
+size_t handleResponse(char * data, size_t size)
+{
+    respond.data-> append(data, size* aramemb)
+    return size*n_aramemb;
+
+}
+int main()
+{
+
+    string companyname= "train central";
+    string ownername= "ram";
+    string owneremail="ram@abc.edu";
+    string accessCode= "FKDLjg";
+
+    json:: value jsonData;
+
+    jsondata[companyname]="companyname";
+    jsondata[ownername]="ownername";
+    jsondata[owneremail]="owneremail";
+    jsondata[accesscode]="accesscode";
+
+    string json string= json.data.toStyled string();
+    int a,b,c,d,e;
+
+
+cout<<"company name:";
+cin>>a;
+cout<<"owner name:";
+cin>>b;
+cout<<"roll number";
+cin>>c;
+cout<<"owner email:"
+cin>>d;
+cout<<"access code";
+cin>>e;
+
+}
